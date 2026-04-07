2ed1ae2a8e087a4b3be44faefaaca1beba1d3ba0
Tanya Strydom <tanya.strydom@icloud.com>
2026-04-07 12:51:41 +0100

    🐎 set up for sampling S and C

--- main.jl
+++ main.jl
@@ -11,6 +11,7 @@ Main workflow:
 
 # Load Dependencies
 using CSV
+using DataFrames
 using DifferentialEquations
 using Distributions
 using EcologicalNetworksDynamics
@@ -28,58 +29,75 @@ import Random
 Random.seed!(66);
 
 # --- Global Params ---
+n_networks = 10                    # number of networks to make
 t = 5                              # relaxation time after perturbation
 survival_threshold = 1e-3          # extinction threshold
-S = 10                             # number of species
-C = 0.1                            # connectance
+S_min = 20                         # minimum number spp
+S_max = 60                         # max number spp               
+C_min = 0.05                       # minimum connectance
+C_max = 0.25                       # max connectance      
 
-# --- 2. Build Network ---
+# --- Network param distributions ---
+S_dist = truncated(Normal(40, 10), S_min, S_max);
+C_dist = truncated(Normal(0.15, 0.05), C_min, C_max);
 
-fw = Foodweb(:niche; S, C)
+for i in 1:n_networks
 
-params = default_model(
-    fw,
-    BodyMass(; Z = 10),
-    ClassicResponse(; h = 2),
-)
+    # --- 1. Get network params ---
+    
+    S = round(Int, rand(S_dist))
+    C = rand(C_dist)
 
-A = params.A
+    # --- 2. Build Network ---
 
-# Initial biomasses
-B0 = rand(Uniform(0.1, 1), S)
+    fw = Foodweb(:niche; S, C)
 
-# --- 3. Initial Burn-in ---
-
-sol = simulate(params, B0, t;
-    callback = CallbackSet(
-        extinction_callback(params, survival_threshold)
+    params = default_model(
+        fw,
+        BodyMass(; Z = 10),
+        ClassicResponse(; h = 2),
     )
-)
 
-final_biomasses = sol.u[end]
+    A = params.A
 
-# --- 4. Extract topologically relevant network ---
-# alive and connected species
+    # Initial biomasses
+    B0 = rand(Uniform(0.1, 1), S)
 
-survivors, final_adj_matrix = extract_valid_network(A, final_biomasses, survival_threshold)
+    # --- 3. Initial Burn-in ---
 
-# Stop if system collapsed during burn-in
-if survivors === nothing
-    error("All species extinct after burn-in")
+    sol = simulate(params, B0, t;
+        callback = CallbackSet(
+            extinction_callback(params, survival_threshold)
+        )
+    )
+
+    final_biomasses = sol.u[end]
+
+    # --- 4. Extract topologically relevant network ---
+    # alive and connected species
+
+    survivors, final_adj_matrix = extract_valid_network(A, final_biomasses, survival_threshold)
+
+    # Stop if system collapsed during burn-in
+    if survivors === nothing
+        error("All species extinct after burn-in")
+    end
+
+    # Build network object
+    N = build_network(final_adj_matrix)
+
+    # --- 5. Topological Extinctions ---
+
+    topo_results = run_topological_extinctions(N, params)
+
+    # --- 6. Dynamic Extinctions ---
+
+    dynamic_results = run_dynamic_extinctions(params, final_biomasses)
+
+    # --- 7. Get Robustness (R50) ---
+
+    R_topo = compute_robustness(topo_results)
+    R_dyn  = compute_robustness(dynamic_results)
+
+
 end
-
-# Build network object
-N = build_network(final_adj_matrix)
-
-# --- 5. Topological Extinctions ---
-
-topo_results = run_topological_extinctions(N, params)
-
-# --- 6. Dynamic Extinctions ---
-
-dynamic_results = run_dynamic_extinctions(params, final_biomasses)
-
-# --- 7. Get Robustness (R50) ---
-
-R_topo = compute_robustness(topo_results)
-R_dyn  = compute_robustness(dynamic_results)
