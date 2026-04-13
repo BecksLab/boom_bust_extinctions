51f5a0591d4c5bc86423b4cd7cdd144eb3a142ad
Tanya Strydom <tanya.strydom@icloud.com>
2026-04-13 17:15:01 +0100

    🐣 basic plotting

---
+++ plotting.R
@@ -0,0 +1,40 @@
+# libraries
+library(genzplyr)
+library(tidyverse)
+
+df <- read.csv("outputs/robustness_summaries.csv") %>%
+  pivot_longer(
+    cols = -c(net_id, S, C),
+    names_to = c(".value", "scenario"),
+    names_pattern = "^(topo|dyn)_(.*)$")
+
+ggplot(df) +
+  geom_abline(slope = 1) +
+  geom_point(aes(x = topo,
+                 y = dyn)) +
+  facet_wrap(vars(scenario)) +
+  xlim(0,0.5) +
+  theme_classic()
+
+ggplot(df %>%
+         pivot_longer(-c(net_id, S, C, scenario),
+                      names_to = "extinction")) +
+  geom_boxplot(aes(y = scenario,
+                   x = value,
+                   colour = extinction)) +
+  coord_flip() +
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+curves_df <- read.csv("outputs/extinction_curves.csv")
+
+ggplot(curves_df) +
+  geom_abline(slope = -1,
+              intercept = 1) +
+  geom_point(aes(x = primary,
+                 y = secondary,
+                 colour = type),
+             alpha = 0.2,
+             shape = 15) +
+  facet_wrap(vars(scenario)) +
+  theme_classic()
